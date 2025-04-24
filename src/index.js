d89f7226e81204b53f4546cd73a442cd7e59690b
Катерина Чакмазова <chakmazova06@gmail.com>
2025-04-24 19:02:01 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let number = 1;
+let dividers=0;
+
+
+outerNumber: for (; number<=100; number++) {
+  let half = Math.trunc(number / 2); 
+
+  for (let i=1; i<=half; i++) {
+  if(number%i===0) {
+    dividers++;}
+  if(dividers>1 || number === 1) {
+      dividers=0;
+      continue outerNumber;
+    }
+  }
+  if (number !==1) {
+    console.log(number);  
+  }
+  dividers=0;
+}
